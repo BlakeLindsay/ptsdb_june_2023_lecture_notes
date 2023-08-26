84ee2925c663089fd3ecad52e151167429e69977
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-08-26 17:09:24 -0600

    got pokemon data

--- Unit_4/PokemonChallenge/pokemon.js
+++ Unit_4/PokemonChallenge/pokemon.js
@@ -1,8 +1,16 @@
 let form = document.querySelector('form');
 
+let apiTarget = 'https://pokeapi.co/api/v2/pokemon/';
+
 function getPokemon(event) {
 	event.preventDefault();
-	console.log(event.name.value);
+	console.log(form.input.value);
+	fetch(apiTarget + form.input.value)
+	.then(response => response.json())
+	.then(data => {
+		console.log(data);
+	})
+	.catch(error => console.error("pokemon problem", error))
 }
 
 form.addEventListener('submit', getPokemon);
