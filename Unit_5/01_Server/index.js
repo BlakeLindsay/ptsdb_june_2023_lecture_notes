5a72fb790067ad7c732cbfb0b3d53a37d8363170
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-09-02 14:01:46 -0600

    listened

--- Unit_5/01_Server/index.js
+++ Unit_5/01_Server/index.js
@@ -4,3 +4,18 @@
 console.log('This is my first server!');
 
 // package.json can be modified, however the 'package-lock.json' will never be modified directly by us
+
+// first run 'npm install express'
+// giving myself access to the express package
+const express = require('express');
+// require my application to have the information from "express" in order to run. we can think of this like linking a file in HTML
+
+// initialize a new express instance
+const app = express();
+// give me a new instance of an express application, stored within my "app" variable
+
+// Start our server
+// in order to start out server, we need our application to "listen" on a specific part of our computer
+app.listen(4000, () => console.log(`App is listening on port 4000`));
+// start our server listening for any interaction (think of this like an "event listener" in HTML DOM), and will be located at 'localhost:4000'
+
