e03de3bcb6e0bba44a0a9cb934eba631b47f8171
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-09-02 16:22:28 -0600

    controller get and export

--- Unit_5/01_Server/controllers/practiceController.js
+++ Unit_5/01_Server/controllers/practiceController.js
@@ -1,10 +1,16 @@
 // start by creating something called a router
 // this will be used to help navigate and control endpoints
-const express = require('express'); // make sure I have access to express
-const router = express.Router(); // make a new instance of express' router
-
+// const express = require('express'); // make sure I have access to express
+// const router = express.Router(); // make a new instance of express' router
+const router = require('express').Router();
 
 // create some endpoints using this router
-
+// basic GET route '/'
+router.get('/', (request, response) =>{
+	response.send('Practice Controller');
+});
 
 //make this router available outside of this file
+module.exports = router;
+//export the router variable from this module. This means if we were to 'require' this file, we would be giving the 'require' our router
+// Whatever value is assigned to the module.exports is what we will export, if we have an object, it means we are sending out an object
