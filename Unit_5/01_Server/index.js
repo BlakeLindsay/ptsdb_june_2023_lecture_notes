7b87bf7c092e93d470fe288a1b96ccbb9e181ceb
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-09-02 13:39:03 -0600

    unit 5

---
+++ Unit_5/01_Server/index.js
@@ -0,0 +1,6 @@
+// This file will be my "homepage" for the server
+// This is defined within my "package.json" file
+
+console.log('This is my first server!');
+
+// package.json can be modified, however the 'package-lock.json' will never be modified directly by us
