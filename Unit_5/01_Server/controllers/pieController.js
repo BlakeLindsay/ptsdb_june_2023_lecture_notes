f89c73fcb030231bb3670e9777e0a3bd42dfdda6
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-09-09 18:40:03 -0600

    class over

--- Unit_5/01_Server/controllers/pieController.js
+++ Unit_5/01_Server/controllers/pieController.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 // const router = require('express').Router(); // also works
 const pies = [];
+let currentId = 1;
 
 /* 
 	{
@@ -30,10 +31,42 @@ router.get('/', (req, res) => {
 router.post('/new', (req, res) => {
 	// POST - used to send information from a client to the server
 	console.log(req.body); // main content of the request
-	pies.push(req.body);
+	// pies.push(req.body);
+
+	// TODO Validate that our filling is a string, our crust is a string, and our size is a number before adding the new pie to our array, if something is input with an improper type tell the client what is wrong.
+	let newPie = {
+		id: currentId,
+		filling: req.body.filling,
+		crust: req.body.crust,
+		size: req.body.size
+	};
+	pies.push(newPie);
+	currentId++;
 	//the response is not a return, the function will keep running after a response is sent to the client
-	res.status(202).json({message: "new pizza"});
-})
+	res.status(202).json({message: "new pizza", newPie});
+});
+
+//			paremeter- id
+router.get('/single/:id', (req, res) => {
+	// can add multiple parameters by adding a '/:paramName'
+	// console.log(req.body);
+	//? URL Parameters
+	console.log(req.params);
+	// find my pie with a certain id in my pies array
+	let pieFound = false;
+	pies.forEach((pie) => {
+		if(pie.id == req.params.id) {
+			res.status(200).json({pie});
+			pieFound = true;
+		}
+	});
+
+	if(!pieFound) {
+		res.status(404).json({message: 'pie not found'});
+	}
+
+	// res.send('single pie'); //temp response to avoid timeout
+});
 
 // STEP 3: export our router (make it available in other files)
 module.exports = router;
