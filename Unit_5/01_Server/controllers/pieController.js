53a3ae5be595f3844e81767282b4fd7ec8298a41
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-09-13 19:20:41 -0600

    unit 6

--- Unit_5/01_Server/controllers/pieController.js
+++ Unit_5/01_Server/controllers/pieController.js
@@ -11,14 +11,14 @@ let currentId = 1;
 		filling:'blueberry',
 		crust: 'graham cracker',
 		size: 14
-	} 
+	}
 
 	{
 		id: 2,
 		filling:'blueberry',
 		crust: 'graham cracker',
 		size: 14
-	} 
+	}
 */
 
 // STEP 2: Add endpoints to our new router
@@ -66,6 +66,58 @@ router.get('/single/:id', (req, res) => {
 	}
 
 	// res.send('single pie'); //temp response to avoid timeout
+});
+
+// Edit a pie using HTTP PUT method
+// PUT - HTTP method to edit information
+router.put('/single/:id', (request, response) => {
+	//* I am allowed to use the same endpoing multiple times AS LONG AS the method is different (put vs get etc.). This can be spelled any way as long as I use it consistently AND do not overwrite a JS keyword.
+
+	// how to edit my pie
+	// 1. get the index of the pie to edit, or the pie itself
+	// my id will NOT always match the position of the pie
+	let pieFound = false;
+
+	pies.forEach((pie) => {
+		if (pie.id == request.params.id) {
+			pie.filling = request.body.filling;
+			pie.crust = request.body.crust;
+			pie.size = request.body.size;
+			response.status(200).json({ message: `Pie ${request.params.id} has been updated`, pie });
+			pieFound = true;
+		}
+	});
+
+	if(!pieFound) {
+		response.status(404).json({message: 'pie not found'});
+	}
+
+	// 2. gather the information from postman
+
+	// 3. set the old keys of our pie to the new values from postman
+});
+
+// DELETE method - remove an item(s)
+router.delete('/single/:id', (request, response) => {
+	let pieIndex = -1;
+	// forEach to find our pie
+	pies.forEach((pie, index) => {
+		// keep track of the index when we find a matching id
+		if (pie.id == request.params.id) {
+			pieIndex = index;
+		}
+	});
+
+	// we can remove a certain index from an array, as long as we know what index to remove
+	//? how do I know my pieIndex has been updated?
+	//* if I start my pieIndex at -1, whenever that value still reads '-1' I know it has not been changed, since arrays will never have an index lower than '0'
+	if (pieIndex >= 0) {
+		//remove my pie from the pies array
+		pies.splice(pieIndex, 1);
+		response.status(200).json({message: "pie deleted"});
+	} else {
+		response.status(404).json({error: "No pie deleted"});
+	}
 });
 
 // STEP 3: export our router (make it available in other files)
